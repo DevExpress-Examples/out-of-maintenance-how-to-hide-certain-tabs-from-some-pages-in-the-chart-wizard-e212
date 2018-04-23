487548a0fffbb1a5215c099d785999fbb4d04bac
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Windows.Forms;
+
+using DevExpress.XtraCharts.Wizard;
+// ...
+
+
+namespace docChartWizardHideTabs {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create a new Wizard.
+            ChartWizard wizard = new ChartWizard(this.chartControl1);
+
+            // Obtain a Data page.
+            WizardDataPage page = wizard.DataPage;
+
+            // Hide datasource-related tabs on the Data page.
+            page.HiddenPageTabs.Add(DataPageTab.AutoCreatedSeries);
+            page.HiddenPageTabs.Add(DataPageTab.SeriesBinding);
+
+            // Invoke the Wizard window.
+            wizard.ShowDialog();
+        }
+    }
+}
